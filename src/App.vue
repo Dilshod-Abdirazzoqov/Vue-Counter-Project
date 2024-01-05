66ea6a725390f06887e91f6561be8a2923de1e23
Dilshod <example@gmail.com>
2024-01-05 12:05:49 +0500

    Vue-Counter-Project-With-Vite-FrontendTool

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="container">
+      <h1>Counter: {{counter}}</h1>
+
+      <div class="d-flex justify-content-between mx-3">
+        <button type="button" class="btn btn-success" @click="inc">Add</button>
+        <button type="button" class="btn btn-warning" @click="dec">Delete</button>
+      </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        counter: 0
+      }
+    },
+    methods: {
+      inc () {
+        this.counter +=1;
+      },
+
+      dec () {
+        this.counter -=1;
+      }
+    }
+  }
+</script>
+
+
+
+<style>
+#container{
+   width:20rem;
+   margin:15rem auto;
+   text-align:center;
+}
+
+#container h1{
+  border: 1px solid #eee;
+}
+</style>
